1e5430e97ee8cf1795d835ba82b332483482cbc9
pushparajdev05 <pushparajdev05@gmail.com>
2024-03-04 09:58:45 +0530

    set up email completed

---
+++ javascript/email.js
@@ -0,0 +1,21 @@
+var btn=document.getElementById("email");
+var text=document.getElementsByTagName('input');
+// console.log(descript);
+btn.addEventListener("click",()=>{
+    var name1=text[0].value;
+    var email=text[1].value;
+    var sub=text[2].value;
+    var descript=document.getElementById("area").value;
+    
+    Email.send({
+        SecureToken : "1ad2ea6a-37f5-467d-9972-3e6f8aaf27ad",
+        To : 'pushparajdev05@gmail.com',
+        From : "pushparajdev05@gmail.com",
+        Subject : sub,
+        Body : `name : ${name1} <br>
+        Email : ${email} <br>
+        description : ${descript}`
+    }).then(
+      message => alert("the mail has sent")
+    );
+});
