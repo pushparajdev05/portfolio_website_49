0389e7ab2309695a52ba324f2f2003da99dd3a18
pushparajdev05 <pushparajdev05@gmail.com>
2024-08-08 00:37:22 +0530

    scroll animation completed

--- javascript/typed_script.js
+++ javascript/typed_script.js
@@ -1,3 +1,30 @@
+const slideLeft = {
+    delay: 375,
+    duration: 1250,
+    distance: "130px",
+    origin:"left"
+};
+const slideRight = {
+    delay: 375,
+    duration: 1250,
+    distance: "130px",
+    origin:"right"
+};
+const slideUp = {
+    delay: 375,
+    duration: 1250,
+    distance: "130px",
+    origin:"top"
+};
+
+//constructor
+ScrollReveal({ reset: false });
+
+//slide reveal
+ScrollReveal().reveal('.slideLeft',slideLeft);
+ScrollReveal().reveal('.slideRight',slideRight);
+ScrollReveal().reveal('.slideUp',slideUp);
+
 var typed= new Typed("#type",{
     strings:["Web Developer","Flutter Developer"],
     typeSpeed:50,
